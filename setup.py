98e83cdb1f1365223cc6d7569264a66c9d66e744
pia28 <90255741+k69121@users.noreply.github.com>
2022-05-04 11:22:53 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setiptools
+
+setuptools.setup(
+  include_package_data = True,
+    name='2143134',
+    version = '0.0.1',
+    description = 'oss - dev testaa',
+    author = 'kimbeopgi',
+    author_email = 'k69121@naver.com',
+    ur1 = "https://github.com/k69121/testaa",
+    install_requires=['pytest'],
+    long_description = 'oss - dev testaa python module',
+    long_description_content_type = 'text/markdown',
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "Operating System :: OS Independent",
+    ],
+)
+  
